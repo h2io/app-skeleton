d19a9ed76915f4e8ff13914820676ac6930636ce
Victor Ionescu <victor@dev.ro>
2016-10-04 17:52:54 +0300

    app skeleton example

---
+++ example/App.elm
@@ -0,0 +1,76 @@
+module App exposing (..)
+
+import Html exposing (div, button, text, h1, p, Html)
+import Html.Events exposing (onClick)
+import Html.App as App
+import H2ioAppSkeleton
+
+
+main : Program Never
+main =
+    App.program
+        { init = model ! []
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+appConfig : H2ioAppSkeleton.SkeletonList msg
+appConfig =
+    { header = "Example"
+    , content = div [] []
+    , footer = Nothing
+    , footerLinks = Nothing
+    }
+
+
+type alias Model =
+    { skeleton : H2ioAppSkeleton.Model
+    }
+
+
+type Msg
+    = NoOp
+    | Show
+    | H2ioAppSkeleton (H2ioAppSkeleton.Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+        Show ->
+            let
+                ( skeleton', cmd ) =
+                    H2ioAppSkeleton.update H2ioAppSkeleton.Show model.skeleton
+            in
+                { model | skeleton = skeleton' } ! []
+
+        H2ioAppSkeleton msg' ->
+            let
+                ( skeleton', cmd ) =
+                    H2ioAppSkeleton.update msg' model.skeleton
+            in
+                { model | skeleton = skeleton' } ! []
+
+
+model : Model
+model =
+    { skeleton = H2ioAppSkeleton.model
+    }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Show ] [ text "Show Modal" ]
+        , H2ioAppSkeleton.view appConfig model.skeleton |> App.map H2ioAppSkeleton
+        ]
